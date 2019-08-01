050ed045fbd9f2b194e90d746e7cf9cde7f3b194
Alexandr <alexandr.ponomarenko@icloud.com>
2019-08-01 21:11:18 +0300

    lesson 2.2

---
+++ lesson2/part2/es6.js
@@ -0,0 +1,56 @@
+// let man = {
+//   name: "Bob",
+//   greet: function() {
+//     //   this - man
+//     btn.addEventListtener('click', () => {
+//         // this - man
+//     })
+//   }
+//   //   greet: () => {
+//   //     console.log(this);
+//   //   }
+// };
+// man.greet();
+
+// let a = [1, 2, 3];
+// let b = [4, 5, 6];
+
+// // var c = a.concat(b);
+// // var c = [...a, ...b];
+// var f = function(a, b, c) {
+//   return a + b + c;
+// };
+// // console.log(f(1, 2, 3));
+// console.log(f(...a));
+
+// let a = [1, 2, 3];
+// a = [...a, 4];
+
+// let user = {
+//   id: 1,
+//   name: "Bob"
+// };
+
+// user = {
+//   ...user,
+//   name: "NEW NAME",
+//   email: "bob@test.com"
+// };
+// console.log(user);
+
+// let name = "Bob";
+// let man = { name };
+// console.log(man);
+
+// let f = function(name, age) {
+//   let user = { name, age };
+//   return user;
+// };
+// console.log(f("Bob", 20));
+
+// let img = "1.png";
+// // let template = '<img src="' + img + '">';
+// let template = `<img src="${img}">`;
+// console.log(template);
+
+// console.log(a);
